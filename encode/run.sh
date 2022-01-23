2fe4b19812bd772e12dc4d6f9bb5f075dc08b7b8
mataney <matane@allenai.org>
2022-01-23 17:04:23 +0200

    Update run.sh

--- encode/run.sh
+++ encode/run.sh
@@ -1,5 +1,5 @@
 export GOOGLE_APPLICATION_CREDENTIALS="ai2-israel-d3744270e886.json"
 ray down -n gpucluster --yes clust.yaml
 ray up --yes clust.yaml
-ray submit -v clust.yaml encode/encode_text.py --max_tokens_per_batch=512 --output_path none
+ray submit -v clust.yaml encode/encode_text.py --max_tokens_per_batch=16384 --output_path none
 ray down -n gpucluster --yes clust.yaml
