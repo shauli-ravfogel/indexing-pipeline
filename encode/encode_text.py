5e7142ca91ead51a1f0ec5fda1f410a651d90da5
mataney <mataneyal1@gmail.com>
2022-01-25 12:52:44 +0000

    large scale ray

--- encode/encode_text.py
+++ encode/encode_text.py
@@ -1,21 +1,18 @@
 import argparse
-import numpy as np
-from pathlib import Path
-from smart_open import open as sopen
-import torch
-from torch.utils.data.dataloader import DataLoader
-from transformers import AutoTokenizer, AutoModel
-
-from dataset_reader import TextDatasetReader, InputFeatures
-from adaptive_sampler import (
-    MaxTokensBatchSampler,
-    data_collator_for_adaptive_sampler,
-)
-
-import ray
-from google.cloud import storage
-from smart_open import open as sopen # type: ignore
 import time
+
+import numpy as np
+import ray
+import torch
+from google.cloud import storage
+from smart_open import open as sopen  # type: ignore
+from torch.utils.data.dataloader import DataLoader
+from transformers import AutoModel, AutoTokenizer
+
+from adaptive_sampler import (MaxTokensBatchSampler,
+                              data_collator_for_adaptive_sampler)
+from dataset_reader import InputFeatures, TextDatasetReader
+
 
 class Encoder(object):
     """
@@ -23,134 +20,79 @@ class Encoder(object):
     """
     def __init__(self, model):
         self.model = model
-    
+
     def get_hidden_states(self, inputs: InputFeatures):
-    
-        guids = inputs.pop("guid")
-        dict_to_device(inputs, self.model.device)
-        last_hidden_states = self.model(**inputs)[0]
+        with torch.no_grad():
+            dict_to_device(inputs, self.model.device)
+            last_hidden_states = self.model(**inputs)[0]
         return last_hidden_states
-                        
+
     def get_vector(self, inputs: InputFeatures) -> torch.Tensor:
-    
         """ return a vector representation of the sentence """
+        sentence_ids = inputs.pop("guid")
         hidden_states = self.get_hidden_states(inputs)
-        return self.extract_vector_from_hidden_states(hidden_states)
-    
+        return sentence_ids, self.extract_vector_from_hidden_states(hidden_states)
+
     def extract_vector_from_hidden_states(self, hidden_states: torch.Tensor) -> np.ndarray:
-    
         raise NotImplementedError
-        
+
 
 class MeanEncoder(Encoder):
 
     def __init__(self, model):
         super().__init__(model)
-        
+
     def extract_vector_from_hidden_states(self, hidden_states):
-    
         mean = hidden_states.mean(dim=1)
         return mean.detach().cpu().numpy()
-        
+
 class FirstTokenEncoder(Encoder):
 
     def __init__(self, model):
         super().__init__(model)
-        
+
     def extract_vector_from_hidden_states(self, hidden_states):
-    
         return hidden_states[:,0,:].detach().cpu().numpy()
 
 
 def process_dataset(encoder: Encoder, input_path: str, tokenizer, args):
-        dataset = TextDatasetReader(input_path, tokenizer)
-        dataloader = adaptive_dataloader(args, dataset)
-        
-        all_states = []
-        output_path = input_path.replace("/text/", "/states/").replace(".jsonl.gz", ".npy")
-        with sopen(output_path, "wb") as outfh:
-            for inputs in dataloader:
-                H = encoder.get_vector(inputs)
-                all_states.append(H)
+    dataset = TextDatasetReader(input_path, tokenizer)
+    dataloader = adaptive_dataloader(args, dataset)
 
-            all_states = np.concatenate(all_states, axis = 0)
-            print("saving states of shape {} in {}".format(all_states.shape, output_path))
-            np.save(outfh, all_states)
+    all_states = []
+    all_sentence_ids = []
+    for inputs in dataloader:
+        sentence_ids, H = encoder.get_vector(inputs)
+        all_states.append(H.copy())
+        all_sentence_ids.append(sentence_ids.copy())
+
+    all_states = np.concatenate(all_states, axis = 0)
+    all_sentence_ids = np.concatenate(all_sentence_ids, axis = 0)
+
+    np.save(sopen(store_filename(input_path, "states"), "wb"), all_states)
+    np.save(sopen(store_filename(input_path, "sent_ids"), "wb"), all_sentence_ids)
+    del all_states
+    del all_sentence_ids
+    print("done with file", input_path)
 
 @ray.remote(num_gpus=1)
 def process_dataset_ray(encoder: Encoder, input_path: str, tokenizer, args):
-    return process_dataset(encoder, input_path, tokenizer, args)
-                        
+    process_dataset(encoder, input_path, tokenizer, args)
+
 def main(args, fnames):
 
     device = torch.device(f"cuda:{args.cuda_device}" if torch.cuda.is_available() and not args.force_cpu else "cpu")
-    tokenizer, model = initialize_models(device, args)    
-    encoder = FirstTokenEncoder(model)
-    tasks = []
-    
-    for fname in fnames:
-        #process_dataset(encoder, dataloader,fname)
-        tasks.append(process_dataset_ray.remote(encoder, fname, tokenizer, args))
-            
-    start = time.time()
-    results = ray.get(tasks)
-    print("running time", time.time() - start)            
-          
-"""
-def main(args):
-    device = torch.device(f"cuda:{args.cuda_device}" if torch.cuda.is_available() and not args.force_cpu else "cpu")
+    print("device:", device)
     tokenizer, model = initialize_models(device, args)
     encoder = FirstTokenEncoder(model)
-    dataset = TextDatasetReader(args, input_file, tokenizer)
-    
-    
-    files = find_ann_path(args)
-    for input_file in tqdm(files):
-        print("Running on input file: ", input_file)
-        dataset = TextDatasetReader(args, input_file, tokenizer)
-        if not dataset:
-            print("Skipping empty file")
-            continue
-        dataloader = adaptive_dataloader(args, dataset)
+    tasks = []
 
-        batch_idx = 0
-        for inputs in dataloader:
-            with torch.no_grad():
-                guids = inputs.pop("guid")
-                dict_to_device(inputs, device)
-                last_hidden_states = model(**inputs)[0]
-                _, substitutes = last_hidden_states.topk(100)
-                substitutes = clean_subs(special_tokens, inputs["input_ids"], substitutes.cpu())
-                write_substitutes_to_file(
-                    infer_outfile(args.output_path, input_file, batch_idx), guids, inputs, substitutes
-                )
-            batch_idx += 1
-"""
+    for fname in fnames:
+        tasks.append(process_dataset_ray.remote(encoder, fname, tokenizer, args))
 
-def infer_outfile(output_dir, input_file, input_idx):
-    filename = input_file[input_file.rfind("/") + 1 :]
-    return output_dir + f"/{SUBSTITUTES_DIR}/" + filename + f"-{input_idx}"
-
-
-def write_substitutes_to_file(outfile, sent_guids, inputs, substitutes):
-    attention_mask = inputs["attention_mask"].bool()
-
-    sent_lengths = inputs["attention_mask"].sum(1)
-    tokens = inputs["input_ids"].masked_select(attention_mask)
-    substitutes = substitutes.masked_select(attention_mask.cpu().unsqueeze(2)).view(-1, NUM_SUBS_TO_USE)
-
-    write_results(outfile, tokens, sent_lengths, substitutes, sent_guids)
-
-
-def write_results(outfile, tokens, sent_lengths, substitutes, sent_guids):
-    def _write_results(file):
-        np.save(sopen(f"{file}-tokens.npy", "wb"), tokens.cpu().numpy().astype(np.uint16))
-        np.save(sopen(f"{file}-lengths.npy", "wb"), sent_lengths.cpu().numpy().astype(np.int16))
-        np.save(sopen(f"{file}-subs.npy", "wb"), substitutes.cpu().numpy().astype(np.uint16))
-        np.save(sopen(f"{file}-sent_guids.npy", "wb"), np.array(sent_guids, dtype=object))
-
-    try_until_success_or_limit(_write_results, outfile)
-
+    start = time.time()
+    ray.get(tasks)
+    print("running time", time.time() - start)
 
 def dict_to_device(inputs, device):
     if device.type == "cpu":
@@ -171,12 +113,6 @@ def initialize_models(device, args):
     assert tokenizer.vocab_size < 65535  # Saving pred_ids as np.uint16
     return tokenizer, model
 
-
-def adaptive_dataloader(args, dataset):
-    sampler = MaxTokensBatchSampler(dataset, max_tokens=args.max_tokens_per_batch, padding_noise=0.0)
-    dataloader = DataLoader(dataset, batch_size=1, sampler=sampler, collate_fn=data_collator_for_adaptive_sampler,)
-    return dataloader
-
 def collect_paths(directory):
     client = storage.Client()
     bucket = client.get_bucket("ai2i-us")
@@ -185,54 +121,30 @@ def collect_paths(directory):
     for blob in bucket.list_blobs(prefix='SPIKE/datasets/text/{}/'.format(directory)):
         if blob.name.endswith(".jsonl.gz"):
             fnames.append("gs://ai2i-us/"+blob.name)
-    return fnames 
-    
+    return fnames
+
 def adaptive_dataloader(args, dataset):
     sampler = MaxTokensBatchSampler(dataset, max_tokens=args.max_tokens_per_batch, padding_noise=0.0)
     dataloader = DataLoader(dataset, batch_size=1, sampler=sampler, collate_fn=data_collator_for_adaptive_sampler,)
     return dataloader
-    
-        
+
+def store_filename(input_path, object_name):
+    return input_path.replace("/text/", "/states/").replace(".jsonl.gz", f"-{object_name}.npy.gz")
+
 if __name__ == "__main__":
 
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('--dir', type=str, default="wikipedia",
                     help='directory name within https://console.cloud.google.com/storage/browser/ai2i-us/SPIKE/datasets/text/')
-    arg_parser.add_argument(
-        "-o", "--output_path", help="where to write files. On cloud will be best", type=str, required=True
-    )
-    arg_parser.add_argument("--data_dir", help="path to where run files are stored.", type=Path, default="~/wsi")
     arg_parser.add_argument("--max_tokens_per_batch", type=int, required=True)
-    arg_parser.add_argument("--input_suffix", help="glob pattern to use.", type=str, default="*.gz")
-    arg_parser.add_argument(
-        "--split_files_into_n_part",
-        help="If specified, we will break the " "files list into few splits and process them in different runs",
-        type=int,
-    )
-    arg_parser.add_argument(
-        "--files_split_to_process",
-        help="If specified, will process just the files_split_to_process part of files."
-        "This is 1-indexing, e.g., there are 100 files, numbered 0-99, split_files_into_n_part=10, "
-        "files_split_to_process=6, we'll use the 60th-69th files.",
-        type=int,
-    )
     arg_parser.add_argument("--force_cpu", action="store_true", help="Should force cpu even when a gpu is available")
     arg_parser.add_argument("--model_name", type=str, help="huggingface model name", default="bert-base-uncased")
     arg_parser.add_argument("--fp16", action="store_true", help="If specified, use fp16.")
     arg_parser.add_argument("--cuda_device", type=int, choices=[0, 1, 2, 3], default=0)
-    
+
     args = arg_parser.parse_args()
     fnames = collect_paths(args.dir)
-    """
-    if args.split_files_into_n_part:
-        assert args.files_split_to_process and (args.files_split_to_process - 1) < args.split_files_into_n_part
 
-    if not args.output_path.startswith("gs://"):
-        Path(args.output_path + f"/{SUBSTITUTES_DIR}").mkdir(exist_ok=True)
-    
-    print(args)
-    """
     ray.shutdown()
-    #ray.init(local_mode=True)
     ray.init(address="auto")
     main(args, fnames)
--- encode/run.sh
+++ encode/run.sh
@@ -1,5 +1,6 @@
 export GOOGLE_APPLICATION_CREDENTIALS="ai2-israel-d3744270e886.json"
+export TOKENIZERS_PARALLELISM=false
 ray down -n gpucluster --yes clust.yaml
 ray up --yes clust.yaml
-ray submit -v clust.yaml encode/encode_text.py --max_tokens_per_batch=16384 --output_path none
+ray submit -v clust.yaml encode/encode_text.py --max_tokens_per_batch=16384
 ray down -n gpucluster --yes clust.yaml
